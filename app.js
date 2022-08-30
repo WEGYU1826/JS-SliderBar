11d0e9f6de24913a349bb18e0b6ee2f80ff3d3e2
WEGYU1826 <awhmm1826@gmail.com>
2022-08-30 14:38:47 +0300

    inital

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar")
+const closeBtn = document.querySelector(".close-btn");
+
+toggleBtn.addEventListener("click" , () => {
+    sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click" , () => {
+    // sidebar.classList.toggle("show-sidebar");
+    sidebar.classList.remove("show-sidebar");
+})
